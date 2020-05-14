e4ab475c9b60ac69c9c100af489da7e1aa767c15
Dime07 <dimasbkt7@gmail.com>
2020-05-14 09:34:49 +0700

    register,ama login udah

---
+++ app/src/main/java/com/example/percobaan2/profile_name.kt
@@ -0,0 +1,20 @@
+package com.example.percobaan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_halo.*
+
+class profile_name : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_name)
+        setSupportActionBar()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+    }
+}
